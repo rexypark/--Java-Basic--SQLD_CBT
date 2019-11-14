2da6f411792ba62f09fdca6fe0a66158aafd32cb
bitcamp <bitcamp@DESKTOP-QT8CV08>
2019-11-14 18:00:14 +0900

    UserVO_feild array name change

--- SQLD_CBT/src/com/mystudy/sqld_cbt/UserVO.java
+++ SQLD_CBT/src/com/mystudy/sqld_cbt/UserVO.java
@@ -3,70 +3,109 @@ package com.mystudy.sqld_cbt;
 
 public class UserVO {
 		//DB의 MEMBER의 테이블과 동일한 형태 필드 선언
-		//ID, NAME, PASSWORD, PHONE, ADDRESS
-		private String user_id;
-		private String user_name;
-		private String user_pw;
-		private String user_phone;
-		private String user_email;
+		private String id;            //(USER_ID)        ### USER_INFO 컬럼 LIST ###
+		private String name;          //(USER_NAME)
+		private String pw;            //(USER_PW)
+		private String phone;         //(USER_PHONE)
+		private String email;         //(USER_EMAIL)
+		private String seqnum;        //(USER_SEQNUM)
 		
+		private String act;           //(USER_ACT)
+		private String time;          //(USER_TIME)
 		
-		public UserVO(String user_id, String user_name, String user_pw, String user_phone, String user_email) {
+		private String qwestion;      //(QUESTION)       ### EXAM_INFO ###
+		private String answer;        //(ANSWER)
+		private String section;       //(SECTION)
+		private String info;          //(EXAM_SEQNUM)
+		
+		public UserVO(String id, String name, String pw, String phone, String email, String seqnum) {
 			super();
-			this.user_id = user_id;
-			this.user_name = user_name;
-			this.user_pw = user_pw;
-			this.user_phone = user_phone;
-			this.user_email = user_email;
+			this.id = id;
+			this.name = name;
+			this.pw = pw;
+			this.phone = phone;
+			this.email = email;
+			this.seqnum = seqnum;
 		}
 
-		public String getUser_id() {
-			return user_id;
+		public UserVO(String id, String name, String act, String time) {
+			super();
+			this.id = id;
+			this.name = name;
+			this.act = act;
+			this.time = time;
 		}
 
-		public void setUser_id(String user_id) {
-			this.user_id = user_id;
+		public String getId() {
+			return id;
 		}
 
-		public String getUser_name() {
-			return user_name;
+		public void setId(String id) {
+			this.id = id;
 		}
 
-		public void setUser_name(String user_name) {
-			this.user_name = user_name;
+		public String getName() {
+			return name;
 		}
 
-		public String getUser_pw() {
-			return user_pw;
+		public void setName(String name) {
+			this.name = name;
 		}
 
-		public void setUser_pw(String user_pw) {
-			this.user_pw = user_pw;
+		public String getPw() {
+			return pw;
 		}
 
-		public String getUser_phone() {
-			return user_phone;
+		public void setPw(String pw) {
+			this.pw = pw;
 		}
 
-		public void setUser_phone(String user_phone) {
-			this.user_phone = user_phone;
+		public String getPhone() {
+			return phone;
 		}
 
-		public String getUser_email() {
-			return user_email;
+		public void setPhone(String phone) {
+			this.phone = phone;
 		}
 
-		public void setUser_email(String user_email) {
-			this.user_email = user_email;
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public String getSeqnum() {
+			return seqnum;
+		}
+
+		public void setSeqnum(String seqnum) {
+			this.seqnum = seqnum;
+		}
+
+		public String getAct() {
+			return act;
+		}
+
+		public void setAct(String act) {
+			this.act = act;
+		}
+
+		public String getTime() {
+			return time;
+		}
+
+		public void setTime(String time) {
+			this.time = time;
 		}
 
 		@Override
 		public String toString() {
-			return "UserVO [user_id=" + user_id + ", user_name=" + user_name + ", user_pw=" + user_pw + ", user_phone="
-					+ user_phone + ", user_email=" + user_email + "]";
+			return "UserVO [id=" + id + ", name=" + name + ", pw=" + pw + ", phone=" + phone + ", email=" + email
+					+ ", seqnum=" + seqnum + ", act=" + act + ", time=" + time + ", qwestion=" + qwestion + ", answer="
+					+ answer + ", section=" + section + ", info=" + info + "]";
 		}
 		
-		
-		
-		
+	
 }
