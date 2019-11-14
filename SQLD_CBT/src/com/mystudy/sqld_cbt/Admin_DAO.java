2ae08aecee3e79d71b29076372fd6d2a41da0d44
bitcamp <bitcamp@DESKTOP-QT8CV08>
2019-11-14 19:08:41 +0900

    admin_DAO

--- SQLD_CBT/src/com/mystudy/sqld_cbt/Admin_DAO.java
+++ SQLD_CBT/src/com/mystudy/sqld_cbt/Admin_DAO.java
@@ -42,29 +42,75 @@ public class Admin_DAO {
 	
 	
 	//1.현재 접속자 정보
-	
+	public void nowUserList() {
+		List<UserVO> list = new ArrayList<>();
+		//DB연결 - Connection 객체 생성(DB와 연결된)
+				try {
+					conn = DriverManager.getConnection(URL, USER, PASSWORD);
+					
+					//PreparedStatement 객체 생성하고 SQL문 실행
+					StringBuilder sql = new StringBuilder();
+					sql.append("SELECT * ");
+					sql.append("FROM USER_LOG ");
+					sql.append("WHERE USER_ACT = '로그인' ");
+					sql.append("ORDER BY L_USER_ID");
+				
+					pstmt = conn.prepareStatement(sql.toString());
+					
+					rs = pstmt.executeQuery();
+						
+					//SQL문 실행 결과에 대한 처리
+					while (rs.next()) {
+						list.add(new UserVO(
+                                             rs.getString("L_USER_ID"), 
+                                             rs.getString("L_USER_NAME"),
+                                             rs.getString("USER_ACT"),
+                                             rs.getString("USER_TIME")));
+					}
+					
+					System.out.println("list data check : " + list.size());
+					
+					System.out.println("|====================================================================|");
+					System.out.println("|    User ID    |       Name      |   접속 여부    |        접속 시간              |");
+					for (UserVO mvo : list) { 
+						System.out.printf("%10s %16s %23s %29s \n", mvo.getId(), mvo.getName(), mvo.getAct()
+								          , mvo.getTime());
+					
+					}
+					
+					
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} finally {
+					//사용객체 close
+					JDBC_Close.closeConnStmtRs(conn, pstmt, rs);
+				}
+				
+				
+		
+	}// nowUserList End
 	
 	
 	//2.전체 회원정보 list
-	public void userListAll(List<MemberVO> list) {
-		 System.out.println("|=======================================================================|");
-		 System.out.println("| Index |    User ID    |   Name   |  Password  |   Phone   |   E-Mail  |");
-		for (MemberVO mvo : list) {
-			System.out.println("|   " + mvo.getSeqnum() + "\t  " + mvo.getId() + "\t   " + mvo.getName() + "\t  " 
-		                            + mvo.getPassword() +
-					                 mvo.getPhone() + mvo.getEmail());
+	public void userListAll(List<UserVO> list) {
+		 System.out.println("|=====================================================================================================================|");
+		 System.out.println("| Index |       User ID        |       Name      |     Password    |      Phone      |             E-Mail             |");
+		for (UserVO mvo : list) { 
+			System.out.printf("%5s %24s %17s %17s %17s %32s \n", mvo.getSeqnum(), mvo.getId(), mvo.getName()
+					          , mvo.getPw(), mvo.getPhone(), mvo.getEmail());
+
 		}
 				
-		
-	}
+	}// userListAll End
 	
 	
 	//3.장기 미접속자
 	
 	
-
-	public List<MemberVO> selectAll() {
-		List<MemberVO> list = new ArrayList<>();
+	
+	
+	public List<UserVO> selectAll() {
+		List<UserVO> list = new ArrayList<>();
 		
 		//DB연결 - Connection 객체 생성(DB와 연결된)
 		try {
@@ -82,7 +128,7 @@ public class Admin_DAO {
 				
 			//SQL문 실행 결과에 대한 처리
 			while (rs.next()) {
-				list.add(new MemberVO(						  
+				list.add(new UserVO(						  
 									  rs.getString("USER_ID"),
 									  rs.getString("USER_NAME"),
 									  rs.getString("USER_PW"),
