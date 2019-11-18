b7b5c12ef89f9242267c0be64bff681ea6538ca1
bitcamp <bitcamp@DESKTOP-CASDLLQ>
2019-11-18 19:27:40 +0900

    hihi

--- SQLD_CBT/src/com/mystudy/sqld_cbt/CBT_Main.java
+++ SQLD_CBT/src/com/mystudy/sqld_cbt/CBT_Main.java
@@ -10,64 +10,79 @@ import log_in.LogInSignIn;
 import vo.UserVO;
 
 public class CBT_Main {
-	
-	
+
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
 		
+
 		while (true) {
 			
-			int choice = 0;
-			System.out.println("|=======================================================================|");
-			System.out.println("|                                                                (v1.0) |");
-			System.out.println("|                                [환영 합니다]                             |");
-			System.out.println("|                        SQLD 기출문제 CBT 프로그램입니다.                    |");
-			System.out.println("|                                                                       |");
-			System.out.println("|                Kdata(한국데이터 진흥원)에서 주관하는 시험인 SQLD는                              |");
-			System.out.println("|          SQL(Structured Query Language) + D(Developer)의 줄임말로                |");
-			System.out.println("|                    데이터베이스 SQL 국가공인 자격증 입니다                                           |");
-			System.out.println("|-----------------------------------------------------------------------|");
-			System.out.println("|        1. 로그인                 |      2. 회원가입           |      3. 프로그램 종료            |");
-			System.out.println("|-----------------------------------------------------------------------|");
-			System.out.print("   => 번호를 입력해 주세요 !!! : ");
-			choice = scan.nextInt();
-			
-			if(choice == 1) {
+			while (true) {
+				Scanner scan = new Scanner(System.in);
+				int choice = 0;
+				System.out.println("|=======================================================================|");
+				System.out.println("|                                                                (v1.0) |");
+				System.out.println("|                                [환영 합니다]                             |");
+				System.out.println("|                        SQLD 기출문제 CBT 프로그램입니다.                    |");
+				System.out.println("|                                                                       |");
+				System.out.println("|                Kdata(한국데이터 진흥원)에서 주관하는 시험인 SQLD는                              |");
+				System.out.println("|          SQL(Structured Query Language) + D(Developer)의 줄임말로                |");
+				System.out.println(
+						"|                    데이터베이스 SQL 국가공인 자격증 입니다                                           |");
+				System.out.println("|-----------------------------------------------------------------------|");
+				System.out.println(
+						"|        1. 로그인                 |      2. 회원가입           |      3. 프로그램 종료            |");
+				System.out.println("|-----------------------------------------------------------------------|");
+				System.out.print("   => 번호를 입력해 주세요 !!! : ");
 				
-				//버퍼 제거
-				scan.nextLine();
-				//logIn
-				LogInSignIn.logIn();
-				
-				
-				
-				break;
-			} else if(choice == 2) {
-				//버퍼 제거
-				scan.nextLine();
-				
-				LogInSignIn.ConformSignIn();
-				LogInSignIn.logIn();
-				break;
-			} else if (choice == 3) {
-				break;
+				choice = scan.nextInt();
+
+				if (choice == 1) {
+
+					// 버퍼 제거
+					scan.nextLine();
+					// logIn
+					LogInSignIn.logIn();
+					while (true) {
+						System.out.println("1. quiz   2. selfTest 3. exit");
+						choice = scan.nextInt();
+						if (choice == 1) {
+							Quiz.quizStart();
+						} else if (choice == 2) {
+							MockTestDAO.mockTestAll();
+						} else {
+							break;
+						}
+					} // while
+				} else if (choice == 2) {
+					// 버퍼 제거
+					scan.nextLine();
+
+					LogInSignIn.ConformSignIn();
+					//exit입력 시 첫번째 창으로 이동 
+					if(LogInSignIn.exit.equals("exit")) {
+						break;
+					}
+					LogInSignIn.logIn();
+					while (true) {
+						int choice2;
+						System.out.println("1. quiz   2. selfTest 3. exit");
+						choice2 = scan.nextInt();
+						if (choice2 == 1) {
+							Quiz.quizStart();
+						} else if (choice2 == 2) {
+							MockTestDAO.mockTestAll();
+						} else {
+							break;
+						}
+					} // while
+				}
+				if (choice == 3) {
+					System.out.println("로그아웃 되셨습니다.");
+					break;
+				}
+				scan.close();
 			}
+			
 		}
-		
-		while (true) {
-			int choice;
-			System.out.println("1. quiz   2. selfTest 3. exit");
-			choice = scan.nextInt();
-			if(choice == 1) {
-				Quiz.quizStart();
-			} else if (choice == 2){
-			MockTestDAO.mockTestAll();
-			} else {
-			break;
-			}
-		}//while
-		System.out.println("로그아웃 되셨습니다.");
-		
-		scan.close();
-	}//main
+	}// main
 }
--- SQLD_CBT/src/log_in/LogInSignIn.java
+++ SQLD_CBT/src/log_in/LogInSignIn.java
@@ -16,75 +16,79 @@ public class LogInSignIn {
 	private static String user_email = "";
 	private static Scanner scan = new Scanner(System.in);
 	private static UserDAO userDao = new UserDAO();
-	
+	public static String exit = "exit";
 	
 	//로그인 메서드
 	public static void logIn() {
-	String user_id = "";
-	String user_pw = "";
-	
-	Scanner scan = new Scanner(System.in);
-	UserDAO userDao = new UserDAO();
-	DbConn.clearScreen();
-	while (true) {
-		System.out.println("         |================================|");
-		System.out.println("         |    [SQLD CBT Program Login]    |         ");
-		System.out.println("         |                                |         ");
-		System.out.println("         |         ID : _________         |         ");
-		System.out.print("                  ID 입력 : ");
-		user_id = scan.nextLine();
-		DbConn.clearScreen();
-		System.out.println("         |================================|");
-		System.out.println("         |    [SQLD CBT Program Login]    |         ");
-		System.out.println("         |                                |         ");
-		System.out.println("                  ID : " + user_id + "              ");
-		System.out.println("                  PW : _________                  ");
-		System.out.print("                 PW 입력 : ");
-		user_pw = scan.nextLine();
-		DbConn.clearScreen();
-		System.out.println("         |================================|");
-		System.out.println("         |    [SQLD CBT Program Login]    |         ");
-		System.out.println("         |                                |         ");
-		System.out.println("                  ID : " + user_id + "              ");
-		System.out.println("                  PW : " + user_pw + "              ");
+		String user_id = "";
+		String user_pw = "";
 		
-		// DB에서 해당 아이디 비밀번호가 있는지 확인 true이면 
-		// logIn 성공하여 while문 break 아니면 실패하여 로그인 반복
-		if(userDao.checkIdPassword(user_id, user_pw)) {
-			System.out.println("         |                                |         ");
-			System.out.println("         |        [log In Success]        |          ");
+		Scanner scan = new Scanner(System.in);
+		UserDAO userDao = new UserDAO();
+		DbConn.clearScreen();
+		while (true) {
 			System.out.println("         |================================|");
-			UserVO logUser = userDao.userInfo;
-			try {
-				Thread.sleep(2000); // 로그인 Success창 2초 딜레이 준 뒤 다음 스텝으로 넘어간다.)
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+			System.out.println("         |    [SQLD CBT Program Login]    |         ");
+			System.out.println("         |                                |         ");
+			System.out.println("         |         ID : _________         |         ");
+			System.out.print("                  ID 입력 : ");
+			user_id = scan.nextLine();
+			DbConn.clearScreen();
+			System.out.println("         |================================|");
+			System.out.println("         |    [SQLD CBT Program Login]    |         ");
+			System.out.println("         |                                |         ");
+			System.out.println("                  ID : " + user_id + "              ");
+			System.out.println("                  PW : _________                  ");
+			System.out.print("                 PW 입력 : ");
+			user_pw = scan.nextLine();
+			DbConn.clearScreen();
+			System.out.println("         |================================|");
+			System.out.println("         |    [SQLD CBT Program Login]    |         ");
+			System.out.println("         |                                |         ");
+			System.out.println("                  ID : " + user_id + "              ");
+			System.out.println("                  PW : " + user_pw + "              ");
 			
-			//아이디, 이름, 로그인 내용  USER_LOG테이블에 insert
-			UserLogDAO.userLog(logUser.getId(), logUser.getName(), "로그인");
-			break;
-		} else {
-			//실패시 while문 반복
-			System.out.println("         |                                |         ");
-			System.out.println("         |         [log In Fail]          |          ");
-			System.out.println("         |================================|");
-			try {
-				Thread.sleep(2000); // 로그인 Success창 2초 딜레이 준 뒤 다시 ID를 물어본다..)
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			DbConn.clearScreen(); //창 클리어
-			}
-		}
+			// DB에서 해당 아이디 비밀번호가 있는지 확인 true이면 
+			// logIn 성공하여 while문 break 아니면 실패하여 로그인 반복
+			if(userDao.checkIdPassword(user_id, user_pw)) {
+				System.out.println("         |                                |         ");
+				System.out.println("         |        [log In Success]        |          ");
+				System.out.println("         |================================|");
+				UserVO logUser = userDao.userInfo;
+				try {
+					Thread.sleep(2000); // 로그인 Success창 2초 딜레이 준 뒤 다음 스텝으로 넘어간다.)
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+				//아이디, 이름, 로그인 내용  USER_LOG테이블에 insert
+				UserLogDAO.userLog(logUser.getId(), logUser.getName(), "로그인");
+				break;
+			} else {
+				//실패시 while문 반복
+				System.out.println("         |                                |         ");
+				System.out.println("         |         [log In Fail]          |          ");
+				System.out.println("         |================================|");
+				try {
+					Thread.sleep(2000); // 로그인 Success창 2초 딜레이 준 뒤 다시 ID를 물어본다..)
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+					DbConn.clearScreen(); //창 클리어																		
+				}
+			}				
 	}
 		
 	//회원가입 메서드
 	public static void ConformSignIn() {
 		while (true) {
 			System.out.println("회원가입");
+			System.out.println("exit 입력 시 log in 창으로 갑니다.");
 			System.out.print("사용할 ID 입력 >>");
 			user_id = scan.nextLine();
+			if (exit.equals(user_id)) {
+				break;
+			}
 			while (true) {
 				if(LogRegex.isId(user_id) == 0 || (user_id.length() == 0)) {
 					System.out.println("아이디 형식이 맞지 않습니다.");
@@ -149,10 +153,10 @@ public class LogInSignIn {
 			user_email = scan.nextLine();
 			while (true) {
 				if(LogRegex.isEmail(user_email) == 0 || (user_email.length() == 0)) {
-					System.out.println("전화번호 형식이 맞지 않습니다.");
-					System.out.println("010-1234-1234 / 01012341234 / 010.1234.1234");
+					System.out.println("이메일 형식이 맞지 않습니다.");
+					System.out.println("asdf12@asd.com");
 					System.out.println("다시 입력해 주세요.");
-					System.out.print("사용할 전화 입력 >>");
+					System.out.print("사용할 이메일 입력 >>");
 					user_email = scan.nextLine();
 				} else {
 					break;
